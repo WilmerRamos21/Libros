8e6bd3d277b37a4f074489fe5ad50a2307df561e
WilmerRamos21 <ramoszko@gmail.com>
2024-10-10 08:36:55 -0500

    Initial commit

---
+++ src/Libro.java
@@ -0,0 +1,29 @@
+public class Libro {
+    //Atributos
+    String titulo;
+    String autor;
+    String editorial;
+    int paginas;
+
+
+    //Metodos
+        //Constructor
+    public Libro(String titulo, String autor, String editorial, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.paginas = paginas;
+
+    }
+    public void imprimir(){
+        System.out.println("El titulo es: "+this.titulo);
+        System.out.println("El autor es: "+this.autor);
+        System.out.println("la editorial es: "+this.editorial);
+        System.out.println("El numero de paginas es: "+this.paginas);
+        System.out.println("---------------");
+    }
+
+    public int imprimirCubierta(){
+        return this.paginas+5;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola a todos");
+        //Para declarar el objeto
+        //Para instanciar una clase
+    Libro libro1 = new Libro("La odisea", "Homero","Antares", 300);
+    Libro libro2 = new Libro("La iliada", "Homero","Antares", 350);
+    Libro libro3 = new Libro("Narraciones Extraordinarias", "Edgar Allan Poe","Antares", 400);
+    Libro libro4 = new Libro("Baldomera","Alberto Sanches", "Antarares",200);
+    Libro libro5 = new Libro("Viaje al centro de la tierra","Julio Verme","Antarares",500);
+
+
+    libro1.imprimir();
+    libro1.imprimirCubierta();
+    libro2.imprimir();
+    libro2.imprimirCubierta();
+    libro3.imprimir();
+    libro3.imprimirCubierta();
+    libro4.imprimir();
+    libro4.imprimirCubierta();
+    libro5.imprimir();
+    libro5.imprimirCubierta();
+    }
+}
